8136cbf9ca4c591f2b7e6370f996f5c5b741c27c
Graham Perich <graham.perich@gmail.com>
2019-01-10 18:02:28 -0500

    adjust snapshot functions for new data structure

--- constants.js
+++ constants.js
@@ -7,29 +7,29 @@ const RADAR_RELAY_URL = 'https://api.radarrelay.com/v2'
 
 const AIRSWAP_TOKEN_METADATA_URL = 'https://token-metadata.production.airswap.io/tokens'
 
-const TOP_TOKENS_DECIMAL_MAP = {
-  BNB: { decimals: 18, levels: calculateLevels(20) },
-  MKR: { decimals: 18, levels: calculateLevels(0.25) },
-  OMG: { decimals: 18, levels: calculateLevels(65) },
-  ZIL: { decimals: 12, levels: calculateLevels(5000) },
-  ZRX: { decimals: 18, levels: calculateLevels(312) },
-  BAT: { decimals: 18, levels: calculateLevels(715) },
-  LINK: { decimals: 18, levels: calculateLevels(250) },
-  REP: { decimals: 18, levels: calculateLevels(10) },
-  GNO: { decimals: 18, levels: calculateLevels(8) },
-  DAI: { decimals: 18, levels: calculateLevels(100) },
-  MANA: { decimals: 18, levels: calculateLevels(2000) },
-  WTC: { decimals: 18, levels: calculateLevels(83) },
-  BNT: { decimals: 18, levels: calculateLevels(140) },
-  KNC: { decimals: 18, levels: calculateLevels(625) },
-  LRC: { decimals: 18, levels: calculateLevels(2222) },
-  REN: { decimals: 18, levels: calculateLevels(4000) },
-  AST: { decimals: 18, levels: calculateLevels(3333) },
-  LOOM: { decimals: 18, levels: calculateLevels(2000) },
-  FUN: { decimals: 18, levels: calculateLevels(25000) },
-  REQ: { decimals: 18, levels: calculateLevels(4000) },
-  SNT: { decimals: 18, levels: calculateLevels(4000) },
-}
+const TOP_TOKENS_DATA = [
+  { symbol: 'BNB', decimals: 18, levels: calculateLevels(20) },
+  { symbol: 'MKR', decimals: 18, levels: calculateLevels(0.25) },
+  { symbol: 'OMG', decimals: 18, levels: calculateLevels(65) },
+  { symbol: 'ZIL', decimals: 12, levels: calculateLevels(5000) },
+  { symbol: 'ZRX', decimals: 18, levels: calculateLevels(312) },
+  { symbol: 'BAT', decimals: 18, levels: calculateLevels(715) },
+  { symbol: 'LINK', decimals: 18, levels: calculateLevels(250) },
+  { symbol: 'REP', decimals: 18, levels: calculateLevels(10) },
+  { symbol: 'GNO', decimals: 18, levels: calculateLevels(8) },
+  { symbol: 'DAI', decimals: 18, levels: calculateLevels(100) },
+  { symbol: 'MANA', decimals: 18, levels: calculateLevels(2000) },
+  { symbol: 'WTC', decimals: 18, levels: calculateLevels(83) },
+  { symbol: 'BNT', decimals: 18, levels: calculateLevels(140) },
+  { symbol: 'KNC', decimals: 18, levels: calculateLevels(625) },
+  { symbol: 'LRC', decimals: 18, levels: calculateLevels(2222) },
+  { symbol: 'REN', decimals: 18, levels: calculateLevels(4000) },
+  { symbol: 'AST', decimals: 18, levels: calculateLevels(3333) },
+  { symbol: 'LOOM', decimals: 18, levels: calculateLevels(2000) },
+  { symbol: 'FUN', decimals: 18, levels: calculateLevels(25000) },
+  { symbol: 'REQ', decimals: 18, levels: calculateLevels(4000) },
+  { symbol: 'SNT', decimals: 18, levels: calculateLevels(4000) },
+]
 
 function calculateLevels(firstLevel) {
   const results = [firstLevel]
@@ -47,5 +47,5 @@ module.exports = {
   DDEX_URL,
   RADAR_RELAY_URL,
   AIRSWAP_TOKEN_METADATA_URL,
-  TOP_TOKENS_DECIMAL_MAP,
+  TOP_TOKENS_DATA,
 }
--- handler.js
+++ handler.js
@@ -1,5 +1,22 @@
+const { Client } = require('pg')
 const { main } = require('./main')
-const { TOP_TOKENS_DECIMAL_MAP } = require('./constants')
+const { TOP_TOKENS_DATA } = require('./constants')
+
+async function initDb() {
+  if (process.env.STAGE === 'local') {
+    const client = new Client({
+      user: 'postgres',
+      host: 'localhost',
+      database: 'postgres',
+      password: 'example',
+      port: 5432,
+    })
+    await client.connect()
+    return client
+  }
+  console.log('environment not yet supported!')
+  return null
+}
 
 const makeResponse = (response, origin = null) => ({
   statusCode: 200,
@@ -14,13 +31,16 @@ const makeResponse = (response, origin = null) => ({
 
 module.exports = {
   hello: (event, context, callback) => {
-    console.log('-----------------------')
-    console.log(event)
-    console.log('-----------------------')
-    console.log(context)
-    console.log('-----------------------')
-
-    callback(null, makeResponse('hello world'))
+    initDb()
+      .then(db => {
+        const { user, database, port, host } = db
+        console.log('db connection successful!', `user ${user} connected on ${host}:${port} to DB "${database}"`)
+        callback(null, makeResponse('hello world'))
+      })
+      .catch(e => {
+        console.log('db connection failure', e.message)
+        callback(null, makeResponse('hello world'))
+      })
   },
   buy: (event, context, callback) => {
     try {
@@ -51,33 +71,37 @@ module.exports = {
     }
   },
   buyPriceSnapshot: async (event, context, callback) => {
-    const results = {}
-    Object.keys(TOP_TOKENS_DECIMAL_MAP).forEach(symbol => {
-      results[symbol] = {}
-    })
+    const batchTimestamp = Date.now()
+    const snapshots = []
 
     const doWorkForPriceLevel = priceLevel => {
-      const tokens = [...Object.entries(TOP_TOKENS_DECIMAL_MAP)]
+      const tokens = JSON.parse(JSON.stringify(TOP_TOKENS_DATA))
       return new Promise(resolve => {
-        async function workLoop(level) {
-          const tuple = tokens.pop()
-          if (!tuple) {
-            resolve(results)
+        async function workLoop(currentLevel) {
+          const tokenData = tokens.pop()
+          if (!tokenData) {
+            resolve(snapshots)
             return
           }
-          console.log(tokens.length)
-          const [token, data] = tuple
+          const { symbol, decimals, levels } = tokenData
           try {
-            const sortedResponses = await main(token, data.levels[level], 'BUY', data.decimals)
-            results[token][data.levels[level]] = sortedResponses
+            const sortedResponses = await main(symbol, levels[currentLevel], 'BUY', decimals)
+            snapshots.push({
+              symbol,
+              batchTimestamp,
+              data: sortedResponses,
+            })
           } catch (error) {
-            results[token][data.levels[level]] = error
+            // yell in slack that there was a critical error.. this should never happen
+            // if we are in this block, it means the `main` routine is broken.
+            // the main routine sqeulches its own errors and logs them as results.
+            // it should never throw or break control flow
           }
 
-          // sleep for 3s to avoid api rate limits
+          // sleep for 1s to avoid api rate limits
           setTimeout(() => {
-            workLoop(level)
-          }, 3000)
+            workLoop(currentLevel)
+          }, 1000)
         }
         workLoop(priceLevel)
       })
@@ -89,7 +113,55 @@ module.exports = {
       await doWorkForPriceLevel(0)
       await doWorkForPriceLevel(1)
       await doWorkForPriceLevel(2)
-      callback(null, results)
+      callback(null, snapshots)
+    } catch (error) {
+      callback(error, null)
+    }
+  },
+  sellPriceSnapshot: async (event, context, callback) => {
+    const batchTimestamp = Date.now()
+    const snapshots = []
+
+    const doWorkForPriceLevel = priceLevel => {
+      const tokens = JSON.parse(JSON.stringify(TOP_TOKENS_DATA))
+      return new Promise(resolve => {
+        async function workLoop(currentLevel) {
+          const tokenData = tokens.pop()
+          if (!tokenData) {
+            resolve(snapshots)
+            return
+          }
+          const { symbol, decimals, levels } = tokenData
+          try {
+            const sortedResponses = await main(symbol, levels[currentLevel], 'SELL', decimals)
+            snapshots.push({
+              symbol,
+              batchTimestamp,
+              data: sortedResponses,
+            })
+          } catch (error) {
+            // yell in slack that there was a critical error.. this should never happen
+            // if we are in this block, it means the `main` routine is broken.
+            // the main routine sqeulches its own errors and logs them as results.
+            // it should never throw or break control flow
+          }
+
+          // sleep for 1s to avoid api rate limits
+          setTimeout(() => {
+            workLoop(currentLevel)
+          }, 1000)
+        }
+        workLoop(priceLevel)
+      })
+    }
+
+    // don't use Promise.all.. we don't want these to run concurrently
+    // calls are intentionally staggered to avoid API rate limits
+    try {
+      await doWorkForPriceLevel(0)
+      await doWorkForPriceLevel(1)
+      await doWorkForPriceLevel(2)
+      callback(null, snapshots)
     } catch (error) {
       callback(error, null)
     }
